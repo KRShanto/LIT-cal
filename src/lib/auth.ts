d5bdb6eded85d462680ba42bc8afd2afa86c8187
KR Shanto <krshanto2005@gmail.com>
2025-09-17 14:09:53 +0600

    refactor(auth): rename getUser to getAuthUser and add getDbUser function for database user retrieval

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,9 +4,11 @@ import { prisma } from "@/lib/db";
 import { redirect } from "next/navigation";
 
 // Type for the returned user object
-export type AuthUser = NonNullable<Awaited<ReturnType<typeof getUser>>>;
+export type AuthUser = NonNullable<Awaited<ReturnType<typeof getAuthUser>>>;
+// Type for the returned user object from the database
+export type DbUser = NonNullable<Awaited<ReturnType<typeof getDbUser>>>;
 
-export async function getUser() {
+export async function getAuthUser() {
   try {
     // Get token from cookies
     const token = (await cookies()).get("token")?.value;
@@ -29,8 +31,6 @@ export async function getUser() {
         email: true,
         name: true,
         imageUrl: true,
-        publicEmail: true,
-        username: true,
         createdAt: true,
         updatedAt: true,
       },
@@ -47,8 +47,6 @@ export async function getUser() {
       email: user.email,
       name: user.name,
       imageUrl: user.imageUrl,
-      username: user.username,
-      publicEmail: user.publicEmail,
       createdAt: user.createdAt,
       updatedAt: user.updatedAt,
     };
@@ -60,7 +58,35 @@ export async function getUser() {
 
 // Helper to use in Server Components to require authentication
 export async function requireAuth() {
-  const user = await getUser();
+  const user = await getAuthUser();
+
+  if (!user) {
+    redirect("/auth/login");
+  }
+
+  return user;
+}
+
+// Get user from the database
+// This is used in Server Components to get the user from the database
+// This expects the user to be authenticated, otherwise it will redirect to the login page
+export async function getDbUser() {
+  const dbUser = await requireAuth();
+
+  const user = await prisma.user.findUnique({
+    where: { id: dbUser.id },
+    select: {
+      id: true,
+      email: true,
+      name: true,
+      publicEmail: true,
+      imageUrl: true,
+      username: true,
+      timezone: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
 
   if (!user) {
     redirect("/auth/login");
